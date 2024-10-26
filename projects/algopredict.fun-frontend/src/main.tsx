715f5b05a24000974903fc549734658f85f12d53
satishccy <satishchoudhari13579@gmail.com>
2024-10-26 21:09:08 +0530

    removed junk in frontend

--- projects/algopredict.fun-frontend/src/main.tsx
+++ projects/algopredict.fun-frontend/src/main.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/App.css'
-import ErrorBoundary from './components/ErrorBoundary'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
