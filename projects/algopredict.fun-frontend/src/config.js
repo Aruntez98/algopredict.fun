e5ea9f7f9aa97b87124e80d3a066ece7eecca263
satishccy <satishchoudhari13579@gmail.com>
2024-11-05 18:14:12 +0530

    updated contract

--- projects/algopredict.fun-frontend/src/config.js
+++ projects/algopredict.fun-frontend/src/config.js
@@ -1,8 +1,8 @@
 import { AlgoPredictClient } from "./contracts/AlgoPredict";
 import * as algokit from "@algorandfoundation/algokit-utils";
 
-export const APP_ID = 728438235;
-export const APP_ADDRESS = "4QKM5VTA4UJGWBWBN73HBJFX6Z4BHNK3BWF4FSWK64N6E7B7FX3PBRPUP4";
+export const APP_ID = 728439464;
+export const APP_ADDRESS = "6C64VETLAKL4JNL6TQLUD5BYXIAC2FTS47P6FIX5SZY5PHBV5D7PZCUE2Q";
 export const APP_ADMIN = "XVE46DEMVUAPRNUF3MCMFSJ7SDUUDLGDH3M5DYFAYFK7MF4TPMBSPUQRQE";
 
 export const TXN_URL = "https://lora.algokit.io/testnet/transaction/"
