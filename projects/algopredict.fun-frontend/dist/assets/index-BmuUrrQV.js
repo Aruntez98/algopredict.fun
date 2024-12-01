9383fb80da322b9c29fb5bb9fde239975a0fde47
satishccy <satishchoudhari13579@gmail.com>
2024-12-01 16:01:29 +0530

    updated app

--- projects/algopredict.fun-frontend/dist/assets/index-BmuUrrQV.js
+++ projects/algopredict.fun-frontend/dist/assets/index-BmuUrrQV.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-0cXpHmnn.js";import"./index-DUlww41f.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-B-acXzBf.js";import"./index-CHpapWaS.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
--- projects/algopredict.fun-frontend/src/config.js
+++ projects/algopredict.fun-frontend/src/config.js
@@ -1,8 +1,8 @@
 import { AlgoPredictClient } from "./contracts/AlgoPredict";
 import * as algokit from "@algorandfoundation/algokit-utils";
 
-export const APP_ID = 728439464;
-export const APP_ADDRESS = "6C64VETLAKL4JNL6TQLUD5BYXIAC2FTS47P6FIX5SZY5PHBV5D7PZCUE2Q";
+export const APP_ID = 730126047;
+export const APP_ADDRESS = "UHBZLXRTWRKYJHPPPZXVRWUKSKJNEM6M75PIA3OQSLRV6BMRHEDUISGBVA";
 export const APP_ADMIN = "XVE46DEMVUAPRNUF3MCMFSJ7SDUUDLGDH3M5DYFAYFK7MF4TPMBSPUQRQE";
 
 export const TXN_URL = "https://lora.algokit.io/testnet/transaction/"
