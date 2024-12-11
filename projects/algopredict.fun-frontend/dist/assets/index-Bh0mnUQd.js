f462cfef9dabfacffe321e93a1cf043f4a6f6c75
satishccy <satishchoudhari13579@gmail.com>
2024-12-11 13:01:09 +0530

    image dimensions fixed

--- projects/algopredict.fun-frontend/dist/assets/index-Bh0mnUQd.js
+++ projects/algopredict.fun-frontend/dist/assets/index-Bh0mnUQd.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CuugTgRh.js";import"./index-C2-O48pc.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-DIlHQI7v.js";import"./index-jFuBnw6I.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
